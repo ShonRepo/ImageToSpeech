71b73c74e1cab4a6ec630ab238ed066d2adab506
Ivan Nikolaev <i.nikolaev@oblakogroup.ru>
2021-12-23 21:47:51 +0300

    update

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
+# Чтение фото
 from PIL import Image
 import pytesseract
-# import pyttsx3
+
+# Аудио
 import gtts
-from playsound import playsound
+
+# Задержка
 import time
 
+# бот
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram import Bot
 
-from pydub import AudioSegment
 
+# Объявление бота
 bot = Bot('5079044432:AAEzSuphtSBX8R0Fto-JKXADeMbvz9BvZzE')
 dp = Dispatcher(bot)
-# engine = pyttsx3.init()
 
+
+# Система приема сообщений
 @dp.message_handler(content_types=["photo"])
 async def get_photo(message):
-
     file_info = await bot.get_file(message.photo[-1].file_id)
     print('get photo')
 
     await bot.send_message(message.from_user.id, 'Ожидайте...')
 
-    await message.photo[-1].download('info.' + file_info.file_path.split('.')[-1]) # ++
+    await message.photo[-1].download('info.' + file_info.file_path.split('.')[-1])  # ++
     text = read_image('info.' + file_info.file_path.split('.')[-1])
 
     say(text)
@@ -31,24 +35,31 @@ async def get_photo(message):
     file = open('text_to_speech.mp3', 'rb')
     await bot.send_audio(message.from_user.id, file, reply_to_message_id=message.message_id)
     file.close()
-    print('message sended')
+    print('message sent')
 
+
+# Чтение изображения
 def read_image(file):
-  image = Image.open(file)
-  
-  text = pytesseract.image_to_string(image, lang='rus')
+    image = Image.open(file)
 
-  print('final read image')
+    text = pytesseract.image_to_string(image, lang='rus')
 
-  return text
+    print('final read image')
 
+    return text
+
+
+# Проговаривание текста
 def say(text):
-  tts = gtts.gTTS(text, lang='ru')
-  tts.save('text_to_speech.mp3')
-  time.sleep(5)
-  print('audio saved!')
+    tts = gtts.gTTS(text, lang='ru')
+    tts.save('text_to_speech.mp3')
+
+    time.sleep(5)
+
+    print('audio saved!')
 
 
+# Запуск приложения
 if __name__ == '__main__':
     print('bot started')
     executor.start_polling(dp)
